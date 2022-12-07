f53e3181c3ea6c3feffd7a0c871dced46e38e116
CaptainQ-12 <106942422+CaptainQ-12@users.noreply.github.com>
2022-12-07 11:17:07 +0700

    create login and register function

--- src/route/web.js
+++ src/route/web.js
@@ -1,9 +1,16 @@
 import express from "express"
+import loginController from "../controller/loginController"
+import homeController from "../controller/homeController"
 
 let router = express.Router();
 
 const initWebRoute = (app) => {
-
+    router.get("/", loginController.getLoginPage);
+    router.post("/login", loginController.loginHomePage);
+    router.get("/register", loginController.registerPage);
+    router.post("/registered", loginController.registeredUsers);
+    router.get("/home", homeController.getHomePage);
+    return app.use('/', router)
 }
 
 export default initWebRoute;
